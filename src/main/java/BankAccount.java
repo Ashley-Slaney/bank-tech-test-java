f985813ff818e4ab0f8ee3a7d3a5019fdf0de86f
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-25 14:53:52 +0100

    setup before each test block and clean bankaccount class

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -8,15 +8,16 @@ public class BankAccount {
     public void deposit(float amount) {
         if (amount <= 0) {
             throw new ArithmeticException("Must be a positive amount to deposit.");
-        } else {
-            this.balance += amount;
         }
+
+        this.balance += amount;
     }
 
     public void withdraw(float amount) {
         if (balance - amount < 0) {
             throw new ArithmeticException("Your balance cannot go below zero.");
         }
+
         this.balance -= amount;
     }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,33 +1,37 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BankTest {
+public class BankAccountTest {
+    private BankAccount subject;
+
+    @BeforeEach
+    void setup() {
+        subject = new BankAccount();
+    }
+
     @Test
     void testBalanceStartsEmpty() {
-        BankAccount subject = new BankAccount();
         float result = subject.balance();
         assertEquals(0, result);
     }
 
     @Test
     void testDepositMoneyIncreasesBalance() {
-        BankAccount subject = new BankAccount();
         subject.deposit(500);
         float result = subject.balance();
         assertEquals(500, result);
     }
 
     @Test
-    void testDepositIncorrectAmountReturnsMessage() {
-        BankAccount subject = new BankAccount();
+    void testDepositDoesntAllowNegativeAmount() {
         Assertions.assertThrows(ArithmeticException.class, () -> subject.deposit(-500));
     }
 
     @Test
     void testWithdrawMoneyDecreasesBalance() {
-        BankAccount subject = new BankAccount();
         subject.deposit(500);
         subject.withdraw(100);
         float result = subject.balance();
@@ -36,7 +40,6 @@ public class BankTest {
 
     @Test
     void testWithdrawCantMakeBalanceGoBelowZero() {
-        BankAccount subject = new BankAccount();
         Assertions.assertThrows(ArithmeticException.class, () -> subject.withdraw(5000));
     }
 }
